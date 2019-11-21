a1852453dc104075804f0e4a9c729914f54510e0
mamud <mamudseun@gmail.com>
2019-11-21 15:03:31 +0100

    Add edit article

--- controllers/article.js
+++ controllers/article.js
@@ -42,3 +42,68 @@ exports.createArticle = (req, res) => {
         });
     });
 };
+
+exports.getOneArticle = () => {
+    
+    const text = `SELECT * FROM articles ORDER BY createdOn`
+
+    pool.connect((err, client, done) => {
+        if (err) {
+            console.log("Can not connect to the DB" + err);
+        } else {
+            console.log("Successful connection")
+        }
+        client.query(text, (error, result) => {
+        console.log(text)
+            done();
+            if (error) {
+            return res.status(404).json({error});
+            } else {
+            return res.status(200).json({
+            status: 'Success', 
+            data: {
+                id: result.rows[0].id,
+                createdOn: result.rows.createdOn,
+                password: result.rows[0].password,
+                imgurl: result.rows[0].imgurl
+            }
+            })
+        }
+        });
+    });
+};
+
+exports.editArticle = (req, res) => {
+    const _id = req.params.articleId;
+    
+    const title = req.body.title
+    const article = req.body.article
+
+    pool.connect((err, client, done) => {
+        if (err) {
+            console.log("Can not connect to the DB" + err);
+        } else {
+            console.log("Successful connection")
+        }
+        
+        const text = `UPDATE articles SET title = $1, article = $2 WHERE id = ${_id} RETURNING *`
+        const values = [title, article]
+
+        client.query(text, values, (error, result) => {
+          done();
+          if (error) {
+              console.log(error)
+            return res.status(404).json({error});
+          } else {
+          return res.status(200).json({
+            status: 'Success', 
+            data: {
+                message: "Article successfully updated",
+                title: result.rows[0].title,
+                article: result.rows[0].article                
+            }
+          })
+        }
+        });
+    });
+};
